3a96ecf3504130d4b4c8c9ef49077d1ed6a1933c
Viviana <vivianafb15@gmail.com>
2021-09-23 17:29:51 -0300

    Agrege data a los json

--- src/apis/productos.js
+++ src/apis/productos.js
@@ -1,7 +1,7 @@
 import { NoticiasFactoryDAO } from '../models/productos/products.factory';
 import { TipoPersistencia } from '../models/productos/products.factory';
 
-const tipo = TipoPersistencia.SQLITE3;
+const tipo = TipoPersistencia.FileSystem;
 const tableName = 'productos';
 
 class prodAPI {
